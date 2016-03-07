316b8efc788c0f39c8d76e2320a078acdb0779b6
Nguyễn Trường Giang 1 <giangnt1@tinhvan.com>
2016-03-07 13:42:07 +0700

    configed buil gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var less = require('gulp-less');
 var less = require('gulp-less-sourcemap');
-var path = require('path');
+var livereload = require('gulp-livereload');
+var cssmin = require('gulp-cssmin');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
 
-gulp.task('less', function () {
-  return gulp.src('assets/less/*.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('assets/css'));
+// Less to CSS: Run manually with: "gulp build-css"
+gulp.task('build-css', function () {
+    return gulp.src('assets/less/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(gulp.dest('assets/css')).on('error', gutil.log)
+        .pipe(livereload());
 });
 
-gulp.task('watch', function() {
-  gulp.watch('assets/less/*.less', ['less']);
+gulp.task('minify-css', function () {
+    return gulp.src(['assets/css/*.css', '!assets/css/*.min.css'])
+        .pipe(plumber())
+        .pipe(cssmin({
+            keepSpecialComments: false,
+            advanced: false
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('assets/css')).on('error', gutil.log);
 });
 
-gulp.task('default', ['less', 'watch'], function() {
-
+// Default task
+gulp.task('watch', function () {
+	livereload.listen();
+    gulp.watch('assets/less/*.less', ['build-css']);
+    gulp.watch('assets/css/*.css', ['minify-css']);
 });
+
+// Start Watching: Run "gulp"
+gulp.task('default', ['build-css', 'minify-css', 'watch']);
