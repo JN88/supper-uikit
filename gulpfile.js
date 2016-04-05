6b96b3a20c5beebff65eec7637bcdc3c8f40e60a
giangnt6988 <giangnt6988@gmail.com>
2016-04-05 14:49:29 +0700

    edit gulp task to combile less

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ var rename = require('gulp-rename');
 
 // Less to CSS: Run manually with: "gulp build-css"
 gulp.task('build-css', function () {
-    return gulp.src('assets/less/*.less')
+    return gulp.src('./assets/less/*.less')
         .pipe(plumber())
         .pipe(less())
-        .pipe(gulp.dest('assets/css')).on('error', gutil.log)
+        .pipe(gulp.dest('./assets/css')).on('error', gutil.log)
         .pipe(livereload());
 });
 
 gulp.task('minify-css', function () {
-    return gulp.src(['assets/css/*.css', '!assets/css/*.min.css'])
+    return gulp.src(['./assets/css/*.css', '!assets/css/*.min.css'])
         .pipe(plumber())
         .pipe(cssmin({
             keepSpecialComments: false,
@@ -26,14 +26,14 @@ gulp.task('minify-css', function () {
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('assets/css')).on('error', gutil.log);
+        .pipe(gulp.dest('./assets/css')).on('error', gutil.log);
 });
 
 // Default task
 gulp.task('watch', function () {
 	livereload.listen();
-    gulp.watch('assets/less/*.less', ['build-css']);
-    gulp.watch('assets/css/*.css', ['minify-css']);
+    gulp.watch('./assets/less/**/*.less', ['build-css']);
+    gulp.watch('./assets/css/*.css', ['minify-css']);
 });
 
 // Start Watching: Run "gulp"
